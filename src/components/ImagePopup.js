b16e3cf2997f0da4f174d40cb1be3fcc64c2408e
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2021-01-09 14:54:37 +0300

    hotfix: добавить картинкам в карточках атрибут alt

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,7 @@ function ImagePopup({card, onClose}) {
   return (
     <div className={`popup popup_type_image ${card ? 'popup_opened' : ''}`}>
       <div className="popup__wrap">
-        <img src={card.link} alt="" className="popup__image" />
+        <img src={card.link} alt={card.name} className="popup__image" />
         <p className="popup__title-image">{card.name}</p>
         <button type="button" className="popup__button-close" onClick={onClose} />
       </div>
